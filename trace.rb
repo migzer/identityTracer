62732b1b69da8f57479bff93090990c051397a2c
Joubert Miguel <miguel.joubert@epitech.eu>
2017-09-04 17:57:27 +0200

    get name & picUrl OK

--- trace.rb
+++ trace.rb
@@ -3,6 +3,7 @@
 require "selenium-webdriver"
 require "headless"
 require "nokogiri"
+require "catpix"
 
 $progress_bar = 0;
 
@@ -41,13 +42,39 @@ def loadingBar()
   end
 end
 
+def getNamePic(profile, cond)
+     if (cond == 0)
+       img = profile.split("src=")[1].split(" alt=")[0];
+       img[0] = '';
+       img[img.length - 1] = '';
+       return (img);
+     else
+       name = profile.split("fcb")[1].split("</")[0];
+       name[0] = '';
+       name[0] = '';
+       return (name);
+     end
+end
+
+def dispImg(imgUrl)
+  Catpix::print_image URRRRRRRRRRRL,
+                      :limit_x => 1.0,
+                      :limit_y => 0,
+                      :center_x => true,
+                      :center_y => true,
+                      :bg => "white",
+                      :bg_fill => true;
+end
+
 def main()
   phoneNumber = getPhoneNumber();
   loadingTh = Thread.new { loadingBar() };
   profile = fbProfileGet(phoneNumber);
   loadingTh.join;
   print("\n\n");
-  puts(profile);
+  imgUrl = getNamePic(profile ,0);
+  name = getNamePic(profile, 1);
+  puts(imgUrl, name);
 end
 
 main();
